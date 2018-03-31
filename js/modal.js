688bf6e4fd8efda719520ca5d4b826c576be4dad
Nikolay Ivanov <kaladanhit1@gmail.com>
2018-03-31 12:14:15 +0300

    Завершение-1

---
+++ js/modal.js
@@ -0,0 +1,26 @@
+var link = document.querySelector('.button-writeus');
+var popup = document.querySelector('.modal-feedback');
+var close = popup.querySelector('.modal-close');
+var form = popup.querySelector('.feedback-form');
+var username = form.querySelector('.name-input');
+var email = form.querySelector('[name=email]');
+var letter = form.querySelector('[name=letter]');
+
+link.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popup.classList.add('modal-feedback-show');
+  console.log(username);
+  username.focus();
+});
+
+close.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popup.classList.remove('modal-feedback-show');
+});
+
+form.addEventListener('submit', function (evt) {
+  if (!username.value || !email.value || !letter.value) {
+    evt.preventDefault();
+    console.log('нужно ввести имя, e-mail и текст сообщения');
+  }
+});
